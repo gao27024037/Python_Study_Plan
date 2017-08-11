2cf7d4eed60ac37fd650fa81bb0a7e466d26d372
872213775 <872213775@qq.com>
2017-08-12 00:18:57 +0800

    通过爬虫创建100个实例.py 附题目 爬虫时通过判断是否是GZIP来觉得是否要解压缩

--- Test100/__init__.py
+++ Test100/__init__.py
@@ -4,31 +4,33 @@ import urllib2
 import gzip
 import StringIO
 from bs4 import BeautifulSoup
+
 url0 = 'http://www.runoob.com/python/python-exercise-example'
+failed=[]
 for j in range(1, 101):
 
-    url = url0+str(j)+'.html'
-    print url
-    response = urllib2.urlopen(url)
-    #print response.getcode()
-    # cont = response.read()
-    # 网页采用了gzip压缩
-    # cont = StringIO.StringIO(response.read())
-    # gzipper = gzip.GzipFile(fileobj=StringIO.StringIO(response.read()))
-    try:
-        context = gzip.GzipFile(fileobj=StringIO.StringIO(response.read())).read()
-        print 'get data success',j
-    except:
-        print 'get data failed',j
-        context = response.read()
-    soup = BeautifulSoup(context, 'html.parser', from_encoding='utf-8')
+# 爬数据
+    url = url0 + str(j) + '.html'
+    headers = {'Accept-encoding': 'gzip'}
+    request = urllib2.Request(url, headers=headers)
+    html = urllib2.urlopen(request).read()
+    Encode = urllib2.urlopen(request).info().get('Content-Encoding')
+    if Encode == 'gzip':
+        html = gzip.GzipFile(fileobj=StringIO.StringIO(html), mode="r").read()
+    soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
     question = soup.find('strong').parent
 
+# 写数据
     try:
         f = file("Test" + str(j) + ".py", 'w')
-        f.write('# -*- coding:utf-8 -*-')
-        f.write('\n#' + question.get_text().encode('utf-8'))
-        print 'write data success',j
+        f.write('# -*- coding:utf-8 -*- ')
+        question = list('\n# '+question.get_text())
+        for i in range(1, len(question) / 35):
+            question.insert(i * 35, '\n# ')
+        f.write("".join(question).encode('utf-8'))
+        print 'write data success', j
     except:
-        print 'write data failed',j
+        print 'write data failed', j
+        failed.append(j)
 
+print failed
