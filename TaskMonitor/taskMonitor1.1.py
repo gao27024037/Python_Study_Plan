a8a0c7db1907c8591159d216d13ac179fe85a83b
872213775 <872213775@qq.com>
2017-08-31 21:02:06 +0800

    增加备份功能，以防监控进程突然被关掉（比如死机，强制关机），数据丢失，无法记录到时间线中

--- TaskMonitor/taskMonitor1.1.py
+++ TaskMonitor/taskMonitor1.1.py
@@ -40,7 +40,7 @@ def writeRecord(s):
     frecord.write(s)
     frecord.close()
 
-
+#
 def writeTotalTime(t):
     if os.path.exists(task + 'TotalTime.txt'):
         fTotalTime = file(task + 'TotalTime.txt', 'r')
@@ -52,24 +52,47 @@ def writeTotalTime(t):
         fTotalTime.write(addTotalTime(t))
     fTotalTime.close()
 
+# 从备份文件中读入备份数据并写入到记录文件中
+def writeBak2Record(bakFile):
+    bakFile = file(task+'.bak','r')
+    string=bakFile.readline()
+    times=string.split(',')
+    writeRecord(formatTime(float(times[0]),float(times[1])))
+    writeTotalTime(int(float(times[1]) - float(times[0])))
+
+# 把t1,t2时间格式化
+def formatTime(startTime, endTime):
+    return time.strftime('%Y-%m-%d 星期%w %H:%M:%S', time.localtime(startTime)) + "--->" \
+        + time.strftime('%Y-%m-%d 星期%w %H:%M:%S', time.localtime(endTime)) + \
+        " 持续：" + min2Time(int(endTime - startTime)) + '\n'
+
+# 保存备份
+def saveBak(startTime, bakTime):
+    bakFile = file(task + '.bak', 'w')
+    bakFile.write(str(startTime)+','+str(bakTime))
+    bakFile.close()
+
 
 if __name__ == '__main__':
     gap = 60
     task = 'SkyrimSE'
+    # 存在taskName.txt则读入其中的名字 作为进程名
     if os.path.exists('taskName.txt'):
         fname = file('taskName.txt', 'r')
         task = fname.readline()
+    # 循环监控
     while True:
+        # 如果存在备份文件，则读入其中的数据并写入记录文件中，后删除备份文件
+        if os.path.exists(task+'.bak'):
+            writeBak2Record(task+'.bak')
+            os.remove(task+'.bak')
+        # 若进程存在，则纪录开始时间，否则休眠gap秒
         if check_exsit(task + '.exe'):
-            t1 = time.time()
+            startTime = time.time()
+            # 若程序存在，则纪录当前时间，写入备份文件中
             while check_exsit(task + '.exe'):
                 time.sleep(gap)
-            t2 = time.time()
-            s = time.strftime('%Y-%m-%d 星期%w %H:%M:%S', time.localtime(t1)) + "--->" \
-                + time.strftime('%Y-%m-%d 星期%w %H:%M:%S', time.localtime(t2)) + \
-                " 持续：" + min2Time(int(t2 - t1)) + '\n'
-            print s
-            writeRecord(s)
-            writeTotalTime(int(t2 - t1))
+                endTime = time.time()
+                saveBak(startTime,endTime)
         else:
             time.sleep(gap)
