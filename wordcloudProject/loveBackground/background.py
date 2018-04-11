74fa80cacc932082e0ca4acec16c34bdd9bfa8fb
gao27024037 <872213775@qq.com>
2018-04-11 21:45:44 +0800

    生成背景图片小工具

--- wordcloudProject/loveBackground/background.py
+++ wordcloudProject/loveBackground/background.py
@@ -1,4 +1,4 @@
-#create wordcloud for wechat backgroud with my girlfriend
+#create wordcloud for wechat backgroud with my girlfriend:llm
 import random
 
 from wordcloud import WordCloud
@@ -7,23 +7,24 @@ import matplotlib.pyplot as plt
 def grey_color_func(word, font_size, position, orientation, random_state=None,
                     **kwargs):
     if random.random()<0.3:
-        data="hsl(%d, %d%%, %d%%)" % (random.randint(150,240),random.randint(50, 70),random.randint(0,60))
+        data="hsl(%d, %d%%, %d%%)" % (random.randint(200,240),random.randint(0, 10),random.randint(20,60))
     else:
-        data="hsl(%d, %d%%, %d%%)" % (random.randint(270,360),random.randint(80, 100),random.randint(50, 80))
-    print(data)
+        data="hsl(%d, %d%%, %d%%)" % (random.randint(320,360),random.randint(90, 100),random.randint(70,85))
     return data
 
 def get_word_frequency_dict(filename):
     word_frequency_dict={}
     for i in open(filename,'r',encoding='utf-8'):
         line=i.split(' ')
-        if  len(line) is 1:
+        if  len(line) is 1 or line[1] == '\n':
             line.append(random.randrange(2,6))
+        if int(line[1])>10:
+            line[1]=10
         word_frequency_dict[line[0]]=int(line[1])
     return word_frequency_dict
 
 
-def creat_word_cloud(word_frequency_dict):
+def creat_word_cloud(word_frequency_dict,name):
     word_cloud=WordCloud( background_color="white", #背景颜色
                           width=540, height=960, margin=2,
                           prefer_horizontal=1,
@@ -35,11 +36,29 @@ def creat_word_cloud(word_frequency_dict):
                           color_func=grey_color_func
                           ).generate_from_frequencies(word_frequency_dict)
 
-    word_cloud.to_file("girlfriend.jpg")
+    word_cloud.to_file(name+".jpg")
     plt.imshow(word_cloud)
     plt.axis("off")
 
 
 if __name__ == '__main__':
+    print("**********************许多句话背景图片*************************")
+    print()
+    print("*           感谢使用**许多句话背景图片**制作工具")
+    print("*           制作人：大宝贝&小宝贝 —— G ❤ L  ヾ(≧▽≦*)o")
+    print("*")
+    print("*   TXT文件格式要求：1.每行一句话")
+    print("*                    2.话后隔一空格加数字（不大于10）,代表该句话在图中大小")
+    print("*                    3.数字也可不加，表示大小随机")
+    print("*   源码参考地址：https://github.com/gao27024037/Python_Study_Plan/tree/master/wordcloudProject/loveBackground")
+    print("***************************************************************")
+    print('\n')
 
-    creat_word_cloud(get_word_frequency_dict('girlfriend.txt'))
+    file = input("请输入txt文件地址：")
+    jpg = input('请输入输出图片名字：')
+    while True:
+        creat_word_cloud(get_word_frequency_dict(file),jpg)
+        print("\n图片已生成："+jpg+".jpg ,如不满意请按N重新生成，如满意请按Y退出程序。")
+        answer = input('满意吗？（Y/N）')
+        if answer == 'y' or answer == 'Y':
+            exit()
