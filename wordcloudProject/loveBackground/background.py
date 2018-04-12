be5f0d1f1ecc282398ef9aae0f277499abf47652
gao27024037 <872213775@qq.com>
2018-04-12 21:11:19 +0800

    修复读取不同编码格式文本的错误

--- wordcloudProject/loveBackground/background.py
+++ wordcloudProject/loveBackground/background.py
@@ -5,6 +5,9 @@ import wordcloud
 import matplotlib.pyplot as plt
 from click._compat import raw_input
 import os
+import chardet
+import codecs
+
 
 import numpy
 import PIL
@@ -12,9 +15,21 @@ import colorsys
 import matplotlib
 import builtins
 
+#转化文本文件为utf-8 如果不是 重新输入
+def convert_to_utf8(filename):
+    try:
+        content=open(filename,'rb').read()
+        ecode=chardet.detect(content)['encoding']
+        if  ecode[:2]=='GB':
+            ecode='GBK'
+        str=content.decode(encoding=ecode).encode(encoding='utf-8')
+        open(filename,'w',encoding='utf-8').write(str.decode(encoding='utf-8'))
+        return True
+    except:
+        return False
 
-
-def grey_color_func(word, font_size, position, orientation, random_state=None,
+#获取颜色
+def color_func(word, font_size, position, orientation, random_state=None,
                     **kwargs):
     if random.random()<0.3:
         data="hsl(%d, %d%%, %d%%)" % (random.randint(200,240),random.randint(0, 10),random.randint(20,60))
@@ -22,9 +37,13 @@ def grey_color_func(word, font_size, position, orientation, random_state=None,
         data="hsl(%d, %d%%, %d%%)" % (random.randint(320,360),random.randint(90, 100),random.randint(70,85))
     return data
 
+#获取词频
 def get_word_frequency_dict(filename):
+    while convert_to_utf8(file) is False:
+        file = raw_input(file+"文件格式错误，请重新输入正确的文本格式（含文件后缀,如：input.txt）：")
     word_frequency_dict={}
-    for i in open(filename,'r',encoding='utf-8'):
+    file=open(filename, 'r',encoding='utf-8')
+    for i in file:
         line=i.split(' ')
         if  len(line) is 1 or line[1] == '\n':
             line.append(random.randrange(3,6))
@@ -43,16 +62,14 @@ def creat_word_cloud(word_frequency_dict,name):
                             min_font_size=5,
                             font_path='simkai.ttf',
                             random_state=40,
-                          color_func=grey_color_func
+                          color_func=color_func
                           ).generate_from_frequencies(word_frequency_dict)
 
     word_cloud.to_file(name+".jpg")
     plt.imshow(word_cloud)
     plt.axis("off")
 
-
-if __name__ == '__main__':
-    os.system("cls")
+def print_title():
     print("**********************文字背景壁纸*************************")
     print("*")
     print("*           感谢使用**文字背景壁纸**制作工具")
@@ -61,14 +78,24 @@ if __name__ == '__main__':
     print("*   TXT文件格式要求：1.每行一句话,一共20行左右为宜")
     print("*                    2.话后隔一空格加数字（不大于10）,代表该句话在图中大小")
     print("*                    3.数字也可不加，表示大小随机")
+    print("*   如有疑问，请联系原作者：github--gao27024037")
     print("*   源码参考地址：https://github.com/gao27024037/Python_Study_Plan/tree/master/wordcloudProject/loveBackground")
     print("***************************************************************")
     print('\n')
-    #
+
+
+if __name__ == '__main__':
+    os.system("cls")
+    print_title()
+
+    #检测文件
     file = raw_input("请输入txt文件地址（含文件后缀,如：input.txt）：")
     while os.path.exists(file) is False:
         file = raw_input(file+"文件不存在，请重新输入txt文件地址（含文件后缀,如：input.txt）：")
+    while convert_to_utf8(file) is False:
+        file = raw_input(file+"文件格式错误，请重新输入正确的文本格式（含文件后缀,如：input.txt）：")
     jpg = raw_input('请输入输出图片名字(不含后缀，如： output)：')
+
     while True:
         creat_word_cloud(get_word_frequency_dict(file),jpg)
         print("\n图片已生成："+jpg+".jpg ,如不满意请按N重新生成，如满意请按Y退出程序。")
