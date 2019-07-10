f37cfc6442312164405b3c71d2e3fdce625fc701
gao27024037 <872213775@qq.com>
2019-07-10 23:10:42 +0800

    第一章 MNIST机器学习入门

--- Deep-Learning-21-Examples/chapter_1/save_pic.py
+++ Deep-Learning-21-Examples/chapter_1/save_pic.py
@@ -3,7 +3,7 @@ from tensorflow.examples.tutorials.mnist import  input_data
 import scipy.misc
 import os
 
-minst = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 save_dir = 'MNIST_data/raw/'
 if os.path.exists(save_dir) is False:
@@ -11,7 +11,7 @@ if os.path.exists(save_dir) is False:
 
 # keep first 20 images
 for i in range(20):
-    image_array = minst.train.images[i,:].reshape(28,28)
+    image_array = mnist.train.images[i,:].reshape(28,28)
     filename = save_dir+'minst_train_'+str(i)+'.jpg'
     # scipy.misc.toimage(image_array,cmin=0.0,cmax=1.0).save(filename)
     scipy.misc.imsave(filename,image_array)
